80da41d0ca3994cfc363669fe5163b410cd64033
roshan <roshanprusty100@gmail.com>
2023-07-03 20:20:04 +0530

    -

--- Tree/binarytree/Diameter.java
+++ Tree/binarytree/Diameter.java
@@ -3,93 +3,104 @@ package com.Tree.binarytree;
 import java.util.Stack;
 
 public class Diameter {
-        public static class Node{
-            int data;
-            Node left;
-            Node right;
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
 
-            Node(int data, Node left, Node right){
-                this.data=data;
-                this.left=left;
-                this.right=right;
-            }
+        Node(int data, Node left, Node right) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
         }
-        public static class Pair{
-            Node node;
-            int state;
-            Pair(Node node, int state){
-                this.node=node;
-                this.state=state;
-            }
-        }
-        public static void display(Node node){
-            if(node == null) return;
-            String str= "";
-            str+= node.left == null ? "." : node.left.data+"";
-            str+="<-"+node.data+"->";
-            str+=node.right==null? "." : node.right.data +"";
-            System.out.println(str);
+    }
 
-            display(node.left);
-            display(node.right);
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
         }
+    }
+
+    public static void display(Node node) {
+        if (node == null)
+            return;
+        String str = "";
+        str += node.left == null ? "." : node.left.data + "";
+        str += "<-" + node.data + "->";
+        str += node.right == null ? "." : node.right.data + "";
+        System.out.println(str);
+
+        display(node.left);
+        display(node.right);
+    }
+
     public static int diameterOfBinaryTree(Node root) {
-        int[] diameter= new int[1];
+        int[] diameter = new int[1];
         height(root, diameter);
         return diameter[0];
 
     }
-    private static int height (Node root, int[] diameter){
-        if(root == null) return 0;
+
+    private static int height(Node root, int[] diameter) {
+        if (root == null)
+            return 0;
         int left = height(root.left, diameter);
         int right = height(root.right, diameter);
-        diameter[0]=Math.max(diameter[0], left+right);
-        return 1+ Math.max(left, right);
+        // In the code, `diameter[0]=Math.max(diameter[0], left+right);` is used to
+        // update the diameter
+        // of the binary tree.
+        diameter[0] = Math.max(diameter[0], left + right);
+        // The line `return 1+ Math.max(left, right);` is calculating the height of the
+        // current node in
+        // the binary tree.
+        return 1 + Math.max(left, right);
     }
-        public static void main(String[] args) {
-            Integer[] arr={50, 25, 12, null, null, 37, 30, null,
-                    null, null, 75, 62, null, 70, null, null, 87, null, null};
 
-            Node root = new Node(arr[0], null, null);
-            Pair rtp = new Pair(root, 1);
-            Stack<Pair> st = new Stack<>();
-            st.push(rtp);
+    public static void main(String[] args) {
+        Integer[] arr = { 50, 25, 12, null, null, 37, 30, null,
+                null, null, 75, 62, null, 70, null, null, 87, null, null };
 
-            int idx=0;
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+        Stack<Pair> st = new Stack<>();
+        st.push(rtp);
 
-            while (st.size()>0){
-                Pair top = st.peek();
-                if(top.state==1){
-                    //left
-                    idx++;
-                    if(arr[idx]!=null){
-                        top.node.left = new Node(arr[idx],null, null);
-                        Pair lp = new Pair(top.node.left,1);
-                        st.push(lp);
-                    }else{
-                        top.node.left=null;
-                    }
-                    top.state++;
-                } else if (top.state==2) {
-                    //right
-                    idx++;
-                    if(arr[idx]!=null){
-                        top.node.right = new Node(arr[idx],null, null);
-                        Pair rp = new Pair(top.node.right,1);
-                        st.push(rp);
-                    }else{
-                        top.node.right=null;
-                    }
-                    top.state++;
-                }else {
-                    st.pop();
+        int idx = 0;
+
+        while (st.size() > 0) {
+            Pair top = st.peek();
+            if (top.state == 1) {
+                // left
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.left = new Node(arr[idx], null, null);
+                    Pair lp = new Pair(top.node.left, 1);
+                    st.push(lp);
+                } else {
+                    top.node.left = null;
                 }
+                top.state++;
+            } else if (top.state == 2) {
+                // right
+                idx++;
+                if (arr[idx] != null) {
+                    top.node.right = new Node(arr[idx], null, null);
+                    Pair rp = new Pair(top.node.right, 1);
+                    st.push(rp);
+                } else {
+                    top.node.right = null;
+                }
+                top.state++;
+            } else {
+                st.pop();
             }
-            System.out.println(diameterOfBinaryTree(root));
-
-
+        }
+        System.out.println(diameterOfBinaryTree(root));
 
     }
-
 
 }
