728655d0d40f546ff98983e38875d2bca3516b4a
roshan <roshanprusty100@gmail.com>
2023-11-20 14:08:15 +0530

    Lambda

---
+++ OOP6/Lambda/New.java
@@ -0,0 +1,33 @@
+package com.OOP6.Lambda;
+
+import com.OOP6.comparision.LambdaFun;
+
+public class New {
+    public static void main(String[] args) {
+        Operation sum = new Operation() {
+            @Override
+            public int operation(int a, int b) {
+                return a+b;
+            }
+        };
+        Operation subtract = new Operation() {
+            @Override
+            public int operation(int a, int b) {
+                return b-a;
+            }
+        };
+        Operation product = (a,b) -> a*b; //****
+
+        New myCalc= new New();
+        System.out.println(myCalc.operate(25,45,sum));
+        System.out.println(myCalc.operate(25,45,subtract));
+        System.out.println(myCalc.operate(25,45,product));
+
+    }
+    private int operate(int a, int b, Operation op){
+        return op.operation(a,b);
+    }
+}
+interface Operation{
+    int operation(int a, int b);
+}
--- OOPsPratice/Main.java
+++ OOPsPratice/Main.java
@@ -1,10 +1,11 @@
 package com.OOPsPratice;
 
 public class Main {
+    @SuppressWarnings("unchecked")
     public static void main(String[] args) {
-        Class check = new Class();
-        check.func();
-        check.greeting();
-        InterfaceA.newYear();
+//        Class check = new Class();
+//        check.func();
+//        check.greeting();
+//        InterfaceA.newYear();
     }
 }
