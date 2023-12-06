525d217209b1bc23e7b2e68c9c4c3d8cc5b2d4cc
roshan <roshanprusty100@gmail.com>
2023-12-06 10:29:30 +0530

    Template

--- Template/TwoDimensional/Diagonal.java
+++ Template/TwoDimensional/Diagonal.java
@@ -29,10 +29,28 @@ public class Diagonal {
 
         return intArray;
     }
+    static int[] reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
 
+        while (start < end) {
+            // Swap elements at start and end indices
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            // Move indices towards the center
+            start++;
+            end--;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
-        int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] arr = {{1, 2, 3},
+                       {4, 5, 6},
+                       {7, 8, 9}};
 
+        //reverse krna rehe gya.
         System.out.println(Arrays.toString(diagonal(arr)));
     }
 }
